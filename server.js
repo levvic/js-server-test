664109f0f66e161c370e7274e98d80aa1486651a
Victoria Panchenko <vkpanchenko@gmail.com>
2021-10-19 13:44:47 +0200

    added POST and PUT for comments

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ const routes = {
     'PUT': downvoteArticle
   },
   '/comments' : {
-    'POST' : createComment
+    'POST': createComment
   },
   '/comments/:id' : {
-
+    'PUT': updateComment
   },
   '/comments/:id/upvote' : {
 
@@ -254,6 +254,54 @@ function downvote(item, username) {
   return item;
 }
 
+function createComment(url, request) {
+  const requestComment = request.body && request.body.comment;
+  const response = {};
+
+  if (requestComment && requestComment.body &&
+    requestComment.username && database.users[requestComment.username] &&
+    requestComment.articleId && database.articles[requestComment.articleId]) {
+    const comment = {
+      id: database.nextCommentId++,
+      body: requestComment.body,
+      username: requestComment.username,
+      articleId: requestComment.articleId,
+      upvotedBy: [],
+      downvotedBy: []
+    };
+
+    database.comments[comment.id] = comment;
+    database.users[comment.username].commentIds.push(comment.id);
+    database.articles[comment.articleId].commentIds.push(comment.id);
+
+    response.body = {comment: comment};
+    response.status = 201;
+  } else {
+    response.status = 400;
+  }
+
+  return response;
+}
+
+function updateComment(url, request) {
+  const id = Number(url.split('/').filter(segment => segment)[1]);
+  const savedComment = database.comments[id];
+  const requestComment = request.body && request.body.comment;
+  const response = {};
+
+  if (!id || !requestComment || !requestComment.body) {
+    response.status = 400;
+  } else if (!savedComment) {
+    response.status = 404;
+  } else {
+    savedComment.body = requestComment.body;
+
+    response.body = {comment: savedComment};
+    response.status = 200;
+  }
+
+  return response;
+}
 // Write all code above this line.
 
 const http = require('http');
